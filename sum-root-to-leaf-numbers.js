8ab41644b9c410c1033bce0ac33645db40ef799d
Swati <swatiasana@gmail.com>
2024-10-22 12:05:46 -0700

    Add time and space complexity

--- sum-root-to-leaf-numbers.js
+++ sum-root-to-leaf-numbers.js
@@ -1,3 +1,9 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
